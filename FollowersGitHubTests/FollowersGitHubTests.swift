1e0d537a209875e16c0668eadb8e23ac69349ecf
AlanAndCode <alan.andersonbr@gmail.com>
2024-10-18 14:56:21 -0300

    Initial Commit

---
+++ FollowersGitHubTests/FollowersGitHubTests.swift
@@ -0,0 +1,17 @@
+//
+//  FollowersGitHubTests.swift
+//  FollowersGitHubTests
+//
+//  Created by Alan Modesto on 18/10/24.
+//
+
+import Testing
+@testable import FollowersGitHub
+
+struct FollowersGitHubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
