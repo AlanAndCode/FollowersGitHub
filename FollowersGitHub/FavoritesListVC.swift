e0b6fa601313032d06d1a2df1357941eb109ff79
AlanAndCode <alan.andersonbr@gmail.com>
2024-10-22 11:09:45 -0300

    feat: add tabbar for Search and Favorites

---
+++ FollowersGitHub/FavoritesListVC.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesListVC.swift
+//  FollowersGitHub
+//
+//  Created by Alan Modesto on 18/10/24.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("favorites loaded")
+        view.backgroundColor = .systemBlue
+    }
+
+}
