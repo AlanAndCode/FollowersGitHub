6b3a8176ee64956ea84fd93cd5087987820530b1
AlanAndCode <alan.andersonbr@gmail.com>
2024-10-22 11:14:40 -0300

    feat: create a List of Followers View Controller

---
+++ FollowersGitHub/FollowersListVC.swift
@@ -0,0 +1,32 @@
+//
+//  FollowersListVC.swift
+//  FollowersGitHub
+//
+//  Created by Alan Modesto on 21/10/24.
+//
+
+import UIKit
+
+class FollowersListVC: UIViewController {
+    var username: String!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        navigationController?.isNavigationBarHidden = false
+        navigationController?.navigationBar.prefersLargeTitles = true
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
