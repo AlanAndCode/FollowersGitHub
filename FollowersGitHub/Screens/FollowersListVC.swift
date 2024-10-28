935c212b74abba91bb3db426ee000211ddf207cd
AlanAndCode <alan.andersonbr@gmail.com>
2024-10-28 11:43:49 -0300

    feat: add FollowersList Swift

--- FollowersGitHub/Screens/FollowersListVC.swift
+++ FollowersGitHub/Screens/FollowersListVC.swift
@@ -6,27 +6,97 @@
 //
 
 import UIKit
+import Foundation
 
 class FollowersListVC: UIViewController {
-    var username: String!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .systemBackground
-        navigationController?.isNavigationBarHidden = false
-        navigationController?.navigationBar.prefersLargeTitles = true
-
-        // Do any additional setup after loading the view.
+    
+    
+    enum Section {
+        case main
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    var username: String!
+    var followers: [Follower] = []
+    
+    var collectionView: UICollectionView!
+    var dataSource: UICollectionViewDiffableDataSource<Section, Follower>!
+    //inicializa com configuracoes iniciais baseadas no que quero carregar o app
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureViewController()
+        configureCollectionView()
+        getFollowers()
+        configureDataSource()
+        
     }
-    */
+    //inicializador
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    //configurando o ViewController
+    func configureViewController(){
+        view.backgroundColor = .systemBackground
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    //o que é uma collection view? modo de exibicao por colecao ideal para quando temos celulas lado a lado
+    func configureCollectionView(){
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UIHelper.createThreeColumnFlowLayout(in: view))
+        view.addSubview(collectionView)
+        collectionView.delegate = self
+        collectionView.backgroundColor = .systemBackground
+        collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
+    }
+    
+    
+    // consumindo os seguidores
+    func getFollowers(){
+        NetworkManager.shared.getFollowers(for: username, page: 1) { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case.success(let followers):
+                self.followers = followers
+                self.updateData()
+                
+                
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Bad Stuff Happend", message: error.rawValue, buttonTitle: "Ok")
+            }
+        }
+    }
+    
+    //de onde virao as informacoes a serem puxadas
+    func configureDataSource() {
+        dataSource = UICollectionViewDiffableDataSource<Section, Follower>(collectionView: collectionView, cellProvider: { (collectionView, indexPath, follower) -> UICollectionViewCell? in
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FollowerCell.reuseID, for: indexPath) as! FollowerCell
+            cell.set(follower: follower)
+            return cell
+        })
+    }
+    
+    
+    func updateData() {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Follower>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(followers)
+        DispatchQueue.main.async{ self.dataSource.apply(snapshot, animatingDifferences: true)}
+        
+    }
+    
+}
 
+
+extension FollowersListVC: UICollectionViewDelegate {
+    
+    func scrollViewDidZoom(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        let  offsety = scrollView.contentOffset.y
+        let  contentHeight = scrollView.contentSize.height
+        let  height = scrollView.frame.size.height
+        
+        print ("OffsetY = \(offsety)")
+        print ("ContentHeight = \(contentHeight)")
+        print ("Height = \(height) ")
+    }
+    
 }
