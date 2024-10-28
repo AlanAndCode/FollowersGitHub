9f008a31cf07a4003c4e5444efe61bdb32a03ce7
AlanAndCode <alan.andersonbr@gmail.com>
2024-10-28 11:41:18 -0300

    feat: add viewController struct

---
+++ FollowersGitHub/Extensions/UIViewController+est.swift
@@ -0,0 +1,18 @@
+//
+//  UIViewController+est.swift
+//  FollowersGitHub
+//
+//  Created by Alan Modesto on 22/10/24.
+//
+
+import UIKit
+
+extension UIViewController {
+    func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String) {
+        DispatchQueue.main.async {
+            let alertVC = GFAlertVC(title: title, message: message, buttonTitle: buttonTitle)
+            alertVC.modalPresentationStyle = .overFullScreen
+            alertVC.modalTransitionStyle = .crossDissolve
+            self.present(alertVC, animated: true) }
+    }
+}
