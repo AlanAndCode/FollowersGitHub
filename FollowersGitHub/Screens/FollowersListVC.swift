488602c3cb641fca49444b2d73f0de795945444a
AlanAndCode <alan.andersonbr@gmail.com>
2024-11-01 10:03:25 -0300

    feat: add methods to see emptyState, external github and searchController

--- FollowersGitHub/Screens/FollowersListVC.swift
+++ FollowersGitHub/Screens/FollowersListVC.swift
@@ -6,10 +6,20 @@
 //
 
 import UIKit
-import Foundation
+
+
+protocol FollowerListVCDelegate: AnyObject {
+    
+    func didRequestFollowers(for username: String)
+        
+    
+    
+}
 
 class FollowersListVC: UIViewController {
-    
+    protocol FollowerListVCDelegate: AnyObject {
+    func didRequestFollowers(for username: String)
+    }
     
     enum Section {
         case main
@@ -19,16 +29,19 @@ class FollowersListVC: UIViewController {
     var username: String!
     var followers: [Follower] = []
     var hasMoreFollowers = true
-    
+    var filteredFollowers: [Follower] = []
+    var isSearching = false
     var collectionView: UICollectionView!
     var dataSource: UICollectionViewDiffableDataSource<Section, Follower>!
     //inicializa com configuracoes iniciais baseadas no que quero carregar o app
     override func viewDidLoad() {
         super.viewDidLoad()
         configureViewController()
+        configureSearchController()
         configureCollectionView()
         getFollowers(username: username, page: page)
         configureDataSource()
+        
         
     }
     //inicializador
@@ -40,6 +53,7 @@ class FollowersListVC: UIViewController {
     func configureViewController(){
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.hidesSearchBarWhenScrolling = false
     }
     //o que é uma collection view? modo de exibicao por colecao ideal para quando temos celulas lado a lado
     func configureCollectionView(){
@@ -50,18 +64,37 @@ class FollowersListVC: UIViewController {
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
     }
     
+    func configureSearchController() {
+        let searchController = UISearchController()
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.delegate = self
+        searchController.searchBar.placeholder = "Search for a username"
+        searchController.obscuresBackgroundDuringPresentation = false
+        navigationItem.searchController = searchController
+        definesPresentationContext = true // Garantir que a barra de busca apareça corretamente
+    }
     
     // consumindo os seguidores
     func getFollowers(username: String, page: Int){
+        showLoadingView()
         NetworkManager.shared.getFollowers(for: username, page: page) { [weak self] result in
             guard let self = self else { return }
+            self.dismissLoadingView()
             switch result {
             case.success(let followers):
                 if followers.count < 100 {
-                self.hasMoreFollowers = false }
+                    self.hasMoreFollowers = false }
                 self.followers.append(contentsOf: followers)
-                self.updateData()
                 
+                if self.followers.isEmpty {
+                    let message = "This user doesn't have any followers. Go follow them"
+                    DispatchQueue.main.async {
+                        self.showEmptyStateView(with: message, in: self.view)
+                    }
+                    return
+                }
+                
+                self.updateData(on: self.followers)
                 
             case .failure(let error):
                 self.presentGFAlertOnMainThread(title: "Bad Stuff Happend", message: error.rawValue, buttonTitle: "Ok")
@@ -79,7 +112,7 @@ class FollowersListVC: UIViewController {
     }
     
     
-    func updateData() {
+    func updateData(on followers: [Follower]) {
         var snapshot = NSDiffableDataSourceSnapshot<Section, Follower>()
         snapshot.appendSections([.main])
         snapshot.appendItems(followers)
@@ -89,7 +122,7 @@ class FollowersListVC: UIViewController {
     
 }
 
-
+// MARK: - delegate
 extension FollowersListVC: UICollectionViewDelegate {
     
     func scrollViewDidScroll(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
@@ -98,11 +131,57 @@ extension FollowersListVC: UICollectionViewDelegate {
         let height = scrollView.frame.size.height
         
         // Verifica se chegou ao fim e se não estamos carregando seguidores
-        if offsetY > contentHeight - height - 100 {
+        if offsetY > contentHeight - height {
             guard hasMoreFollowers else { return }
             page += 1
             getFollowers(username: username, page: page)
             
         }
     }
+    
+    func collectionView( _ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        let activeArray = isSearching ? filteredFollowers : followers
+        let follower = activeArray[indexPath.item]
+        
+        let destVC = UserInfoVC()
+        destVC.username = follower.login
+        destVC.delegate = self
+        let navController = UINavigationController(rootViewController: destVC)
+        present(navController, animated: true)
+    }
+    
+}
+
+extension FollowersListVC: UISearchResultsUpdating, UISearchBarDelegate {
+    func updateSearchResults(for searchController: UISearchController) {
+        
+        guard let filter = searchController.searchBar.text, !filter.isEmpty else {return}
+        isSearching = true
+        filteredFollowers = followers.filter { $0.login.lowercased().contains(filter.lowercased()) }
+        updateData(on: filteredFollowers)
+        
+    }
+    func
+    searchBarCancelButtonClicked(_
+    searchBar: UISearchBar) {
+        isSearching = false
+        updateData(on: followers)
+    }
+}
+
+extension FollowersListVC: FollowerListVCDelegate{
+    
+    
+    func didRequestFollowers(for username: String) {
+        self.username = username
+        title = username
+        page = 1
+        followers.removeAll()
+        filteredFollowers.removeAll()
+        collectionView.setContentOffset(.zero, animated: true)
+        getFollowers(username: username, page: page)
+    }
+    
+    
 }
